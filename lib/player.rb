4234cf0d21dba0d4f3b56d32c338faa630e6dadb
malin patel <malin.patel@icloud.com>
2016-11-24 12:32:16 +0000

    Implementing hit points

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,18 @@
-class Player
-  attr_reader :name
 
-  def initialize(name)
+class Player
+  attr_reader :name, :hitpoints
+
+  def initialize(name, hitpoints = 100)
     @name = name
+    @hitpoints = hitpoints
+  end
+
+  def attack(player)
+    player.reduce_hitpoints(10)
+  end
+
+  def reduce_hitpoints(number)
+    @hitpoints -= number
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,25 @@
 require 'player'
+
 describe Player do
 
-subject(:player1) {described_class.new("Malin")}
-  describe '#name' do
-    it "should return player's name" do
-      expect(player1.name).to eq "Malin"
+  subject(:player1) {described_class.new("Malin")}
+  subject(:player2) {described_class.new("Veronica")}
+
+    describe '#name' do
+      it "should return player's name" do
+        expect(player1.name).to eq "Malin"
+      end
     end
-  end
+
+    describe 'attack' do
+      it 'checks player1 responds to attack' do
+        expect(player1).to respond_to(:attack)
+      end
+
+      it 'checks player2 hp reduces by 10 points' do
+        expect{player1.attack(player2)}.to change{player2.hitpoints}.by (-10)
+      end
+    end
+
 
 end
